4b888e73e990147d821a5cacb6de11c48831d0c8
akovalev <akovalev@rtech.ru>
2019-02-06 11:28:53 +0000

    Взято отсюда https://gist.github.com/w495/1a40a09bfe50b8841602

---
+++ run.sh
@@ -0,0 +1,2 @@
+#LD_PRELOAD=/usr/local/ffmpeg/lib/libavcodec.so.58;/usr/local/ffmpeg/lib/libavutil.so.56  ./SimpleGoVideoPlayer ../ffobjectmodel-build/app/testVideo.ts
+./SimpleGoVideoPlayer videoplayback.mp4
